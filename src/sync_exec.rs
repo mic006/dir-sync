3dd60180f26763ba2f9aa42b989de2481bba0b7f
mic006 <mic006@users.noreply.github.com>
2026-02-18 18:05:19 +0100

    feat: third pass on sync_exec

--- src/sync_exec.rs
+++ src/sync_exec.rs
@@ -9,10 +9,9 @@ use futures::stream::SelectAll;
 
 use crate::diff::{self, DiffEntry};
 use crate::generic::task_tracker::{TaskExit, TaskTracker};
-use crate::proto::action::FileWriteReq;
 use crate::proto::{
-    ActionReq, MyDirEntry, MyDirEntryExt as _, Specific,
-    action::{DeleteEntryReq, FileReadReq, UpdateMetadataReq},
+    ActionReq, MyDirEntry, MyDirEntryExt as _, PROTO_NULL_VALUE, Specific,
+    action::{DeleteEntryReq, FileReadReq, FileWriteReq, UpdateMetadataReq},
 };
 use crate::tree::{ActionReqSender, ActionRspReceiver, Tree};
 
@@ -20,13 +19,13 @@ use crate::tree::{ActionReqSender, ActionRspReceiver, Tree};
 ///
 /// # Errors
 /// - early exit
-pub fn sync_exec(
+pub async fn sync_exec(
     task_tracker: TaskTracker,
     trees: &mut [Box<dyn Tree + Send + Sync>],
     diff_entries: &[DiffEntry],
 ) -> anyhow::Result<()> {
     let mut ctx = SyncExecCtx::new(task_tracker, trees, diff_entries);
-    ctx.sync_exec()
+    ctx.sync_exec().await
 }
 
 /// Context to update one regular file content
@@ -87,14 +86,52 @@ impl<'a> SyncExecCtx<'a> {
         }
     }
 
-    fn sync_exec(&mut self) -> anyhow::Result<()> {
+    async fn sync_exec(&mut self) -> anyhow::Result<()> {
         // preparation: determine actions to perform
         self.prepare();
 
+        // spawn task to handle incoming streams from all trees
+        let (end_marker_notif_sender, end_marker_notif_receiver) = flume::bounded(1);
+        self.task_tracker.spawn({
+            let tree_receivers = self
+                .trees
+                .iter()
+                .map(|t| t.get_fs_action_responder())
+                .collect::<Vec<_>>();
+            let tree_senders = self.tree_senders.clone();
+            let file_data_ctx = self.file_data_ctx.clone();
+            async move {
+                Self::handle_incoming_streams_task(
+                    tree_receivers,
+                    tree_senders,
+                    end_marker_notif_sender,
+                    file_data_ctx,
+                )
+                .await
+            }
+        })?;
+
         // 1st pass: delete entries, in reverse order to allow deletion of directories
         for diff_entry_idx in self.act_delete.iter().rev() {
-            self.task_tracker.get_shutdown_req_as_result()?;
-            self.entry_delete(*diff_entry_idx)?;
+            self.entry_delete(*diff_entry_idx).await?;
+        }
+
+        // 2nd pass: update entries, per source tree
+        for (src_idx, act_update) in self.act_update.iter().enumerate() {
+            // update all entries for source src_idx
+            for ctx in act_update {
+                self.entry_update(src_idx, ctx).await?;
+            }
+
+            // send end marker to source tree
+            log::debug!("sync_exec[{}]: sending end_marker", src_idx + 1);
+            self.tree_senders[src_idx]
+                .send_async(Arc::new(ActionReq::EndMarker(PROTO_NULL_VALUE)))
+                .await?;
+            // wait for end marker reception
+            let marker = end_marker_notif_receiver.recv_async().await?;
+            debug_assert!(marker == src_idx);
+            // read data have been completely processed
         }
 
         Ok(())
@@ -176,7 +213,7 @@ impl<'a> SyncExecCtx<'a> {
     }
 
     /// Handle deletion of one entry
-    fn entry_delete(&self, diff_entry_idx: usize) -> anyhow::Result<()> {
+    async fn entry_delete(&self, diff_entry_idx: usize) -> anyhow::Result<()> {
         let diff_entry = &self.diff_entries[diff_entry_idx];
         for dst_idx in 0..self.trees.len() {
             if let Some(entry) = &diff_entry.entries[dst_idx] {
@@ -190,21 +227,31 @@ impl<'a> SyncExecCtx<'a> {
                         rel_path: diff_entry.rel_path.clone(),
                     })
                 };
-                self.tree_senders[dst_idx].send(Arc::new(delete_act))?;
+                log::debug!("sync_exec[{}]: delete {}", dst_idx + 1, diff_entry.rel_path);
+                self.tree_senders[dst_idx]
+                    .send_async(Arc::new(delete_act))
+                    .await?;
             }
         }
         Ok(())
     }
 
     /// Handle update of one entry
-    fn entry_update(&self, src_idx: usize, ctx: &FileUpdateCtx) -> anyhow::Result<()> {
+    async fn entry_update(&self, src_idx: usize, ctx: &FileUpdateCtx) -> anyhow::Result<()> {
         let diff_entry = &self.diff_entries[ctx.diff_entry_idx];
 
         if ctx.need_data_read {
             // get file content from source tree
-            self.tree_senders[src_idx].send(Arc::new(ActionReq::ReadFile(FileReadReq {
-                rel_path: diff_entry.rel_path.clone(),
-            })))?;
+            log::debug!(
+                "sync_exec[{}]: read data from {}",
+                src_idx + 1,
+                diff_entry.rel_path
+            );
+            self.tree_senders[src_idx]
+                .send_async(Arc::new(ActionReq::ReadFile(FileReadReq {
+                    rel_path: diff_entry.rel_path.clone(),
+                })))
+                .await?;
             // received data will be forwarded to destination trees
         }
 
@@ -217,7 +264,15 @@ impl<'a> SyncExecCtx<'a> {
             // update metadata on destination trees
             for dst_idx in 0..self.trees.len() {
                 if (ctx.metadata_destination_bitmap & (1 << dst_idx)) != 0 {
-                    self.tree_senders[dst_idx].send(update_act.clone())?;
+                    log::debug!(
+                        "sync_exec[{}->{}]: update metadata for {}",
+                        src_idx + 1,
+                        dst_idx + 1,
+                        diff_entry.rel_path
+                    );
+                    self.tree_senders[dst_idx]
+                        .send_async(update_act.clone())
+                        .await?;
                 }
             }
         }
@@ -243,9 +298,12 @@ impl<'a> SyncExecCtx<'a> {
 
         while let Some((src_idx, event)) = combined_stream.next().await {
             match event {
-                crate::proto::ActionRsp::EndMarker(_) => end_marker_notif_sender.send(src_idx)?,
+                crate::proto::ActionRsp::EndMarker(_) => {
+                    log::debug!("sync_exec[{}]: end_marker received", src_idx + 1);
+                    end_marker_notif_sender.send_async(src_idx).await?;
+                }
                 crate::proto::ActionRsp::Error(error_rsp) => {
-                    log::warn!("sync_exec[{src_idx}]: {error_rsp:?}");
+                    log::warn!("sync_exec[{}]: {error_rsp:?}", src_idx + 1);
                 }
                 crate::proto::ActionRsp::FileData(file_read_rsp) => {
                     // prepare data write request
@@ -256,6 +314,11 @@ impl<'a> SyncExecCtx<'a> {
                         };
                         reg_data.size
                     };
+
+                    let current_segment =
+                        1 + file_read_rsp.offset / file_read_rsp.data.len() as u64;
+                    let nb_segments = file_size.div_ceil(file_read_rsp.data.len() as u64);
+
                     let size = (file_read_rsp.offset == 0).then_some(file_size);
                     let metadata = (file_read_rsp.offset + file_read_rsp.data.len() as u64
                         == file_size)
@@ -267,11 +330,26 @@ impl<'a> SyncExecCtx<'a> {
                         size,
                         metadata,
                     }));
+                    let rel_path = {
+                        let ActionReq::CreateUpdateFile(FileWriteReq { rel_path, .. }) =
+                            &*file_write_req
+                        else {
+                            unreachable!();
+                        };
+                        rel_path
+                    };
 
                     // forward received data to destination trees
                     for dst_idx in 0..tree_senders.len() {
                         if (file_ctx.destination_bitmap & (1 << dst_idx)) != 0 {
-                            tree_senders[dst_idx].send(file_write_req.clone())?;
+                            log::debug!(
+                                "sync_exec[{}->{}]: forward data segment {current_segment}/{nb_segments} for {rel_path}",
+                                src_idx + 1,
+                                dst_idx + 1,
+                            );
+                            tree_senders[dst_idx]
+                                .send_async(file_write_req.clone())
+                                .await?;
                         }
                     }
                 }
